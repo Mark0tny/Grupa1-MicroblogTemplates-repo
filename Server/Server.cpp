960aed6b9bfa4a3fac120c9d1da656cdf42d13e0
Y2J2142 <Y2J2142@hotmail.com>
2018-10-16 22:38:19 +0200

    Server remake to handle Create User request

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,22 +1,42 @@
 #include <pistache/endpoint.h>
-
+#include <pistache/router.h>
+#include <pistache/http.h>
+#include <pistache/net.h>
+#include <iostream>
+#include <memory>
 using namespace Pistache;
 
 
-struct HelloHandler : public Http::Handler
+struct Handler
 {
-    HTTP_PROTOTYPE(HelloHandler)
+    Rest::Router r;
 
-    void onRequest(const Http::Request& rq, Http::ResponseWriter rw)
+    void CreateUser(const Rest::Request& rq, Http::ResponseWriter rw)
     {
-        std::string resp(rq.resource());
-        rw.send(Http::Code::Ok, "Hello world " + resp);
+        std::cout << "Create account request\n";
+        auto email = rq.param(":e").as<std::string>();
+        auto passwd = rq.param(":h").as<std::string>();
+        std::cout << email << " " << passwd << "\n";
+        rw.send(Http::Code::Ok, "Done\n");
+    }
 
+    void setRoutes(Rest::Router& r)
+    {
+        Rest::Routes::Get(r, "/email/:e/hash/:h", Rest::Routes::bind(&Handler::CreateUser, this));
     }
 };
 
 
 int main()
 {
-    Http::listenAndServe<HelloHandler>("*:9080");
+    auto addr = Address(Ipv4(127,0,0,1), Port(9080));
+    Rest::Router router;
+    auto hh = std::shared_ptr<Handler>();
+    hh->setRoutes(router);
+
+    Http::Endpoint server(addr);
+    server.init();
+    server.setHandler(router.handler());
+    server.serve();
+    return 0;
 }

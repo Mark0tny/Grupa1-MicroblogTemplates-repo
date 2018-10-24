a7528a520b6875f0a7a7527c766190b251bd828f
Y2J2142 <Y2J2142@hotmail.com>
2018-10-24 14:54:12 +0200

    request routings added

--- Server/RoutingConsts.hpp
+++ Server/RoutingConsts.hpp
@@ -2,5 +2,10 @@
 
 namespace RoutingConsts
 {
-    std::string create_user_route = "/email/:e/haslo/:h";
+    auto create_user_route = "/email/:e/haslo/:h";
+    auto login_user = "/email/:e/haslo/:h";
+    auto update_user = "/email/:e/nick/:n/haslo/:h";
+    auto follow_blog = "/userid/:uid/blogid/:bid";
+    auto follow_user = "/follower/:followerid/followed/:followedid";
+    auto search = "/searchfor/:sf/searchby/:sb/keyword/:k/orderby/:ob";
 }

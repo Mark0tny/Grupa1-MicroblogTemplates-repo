7939982686ad1e749bc69b6cf1dc50a84078cdf4
Y2J2142 <Y2J2142@hotmail.com>
2018-10-27 20:08:19 +0200

    logging in server and better handling of database connectivity errors

--- Server/classes/RequestHandler.cpp
+++ Server/classes/RequestHandler.cpp
@@ -10,19 +10,22 @@ void RequestHandler::CreateUser(const Rest::Request& rq, Http::ResponseWriter rw
     auto username = rq.param(":n").as<std::string>();
     auto email = rq.param(":e").as<std::string>();
     auto passwd = rq.param(":h").as<std::string>();
-    
+    std::cout << "Create user request\n";   
     auto result = pool.executeQuery(QueriesConsts::find_user, email, username);
-    if(result->size() > 0)
+    if(result.has_value() && result->size() > 0)
         rw.send(Http::Code::Not_Acceptable, "User Already Exists");
-    else
+    else if(result.has_value() && result->size() == 0)
     {
         pool.executeQuery(QueriesConsts::create_user, email, username, passwd);
-        rw.send(Http::Code::Ok, "User Created");
+        rw.send(Http::Code::Created, "User Created");
     }
+    else
+        rw.send(Http::Code::Service_Unavailable, "No Available Connection");
 }
 
 
 void RequestHandler::setRoutes(Rest::Router& r)
 {
+    std::cout << "Routing setup\n";
     Rest::Routes::Post(r, RoutingConsts::create_user_route, Rest::Routes::bind(&RequestHandler::CreateUser, this));
 }

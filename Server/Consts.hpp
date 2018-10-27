d544b9cf7e8673cde3deec839887485cb505c5b3
Y2J2142 <Y2J2142@hotmail.com>
2018-10-27 20:05:44 +0200

    find user query fix

--- Server/Consts.hpp
+++ Server/Consts.hpp
@@ -15,7 +15,7 @@ namespace RoutingConsts
 namespace QueriesConsts
 {
     static const std::string find_user = "find user";
-    static const std::string find_user_query = "SELECT id_user FROM users WHERE email = $1 AND username = $2;";
+    static const std::string find_user_query = "SELECT id_user FROM users WHERE email = $1 OR username = $2;";
     static const std::string create_user = "create user";
     static const std::string create_user_query = "INSERT INTO users (email, username, password) VALUES($1, $2, $3)";
 

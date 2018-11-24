b651265332a1f358f567cc830bf7abe8cc707d97
Y2J2142 <Y2J2142@hotmail.com>
2018-11-25 00:34:31 +0100

    get blogs query update

--- Server/Consts.hpp
+++ Server/Consts.hpp
@@ -37,15 +37,23 @@ namespace QueriesConsts
     constexpr auto login_user_query = "SELECT id_user, username FROM users where ( email = $1 OR username = $1) AND password = $2 LIMIT 1";
     constexpr auto create_microblog = "create microblog";
     constexpr auto create_microblog_query = "INSERT INTO microblog (name, author, tags, private) VALUES($1, $2, $3, $4) RETURNING id_microblog";
+    
     constexpr auto get_my_microblogs = "get users microblogs";
-    constexpr auto get_my_microblogs_query = "WITH T AS (SELECT id_microblog, name, author, tags, private FROM microblog WHERE author = $1) SELECT json_agg(T) FROM T";
+    constexpr auto get_my_microblogs_query = R"(SELECT COALESCE (json_agg(t), '[]'::json) FROM (
+                                                SELECT m.name, u.username, m.time_created, m.id_microblog, u.id_user
+                                                FROM microblog m
+                                                join users u on u.id_user = m.author
+                                                WHERE u.id_user = $1) t)";
+    
     constexpr auto get_posts_by_id = "get posts by id";
-    constexpr auto get_posts_by_id_query = R"(SELECT COALESCE (json_agg(t), '[]'::json) FROM (SELECT u.username, p.views, p.tags, COUNT(c.id_comment), p.time_created, p.id_post, p.id_microblog
+    constexpr auto get_posts_by_id_query = R"(SELECT COALESCE (json_agg(t), '[]'::json) FROM (
+                                            SELECT u.username, p.views, p.tags, COUNT(c.id_comment), p.time_created, p.id_post, p.id_microblog
                                             FROM post p join users u on p.author = u.id_user
                                             join comments c on c.post_id = p.id_post
                                             WHERE p.id_post = $1
                                             GROUP BY u.username, p.views, p.tags, p.time_created, p.id_post, p.id_microblog
                                             ) t)";
+    
     constexpr auto add_post = "add post";
     constexpr auto add_post_query = "INSERT INTO post (author, title, time_created, content, id_microblog, tags, views) VALUES($1, $2, current_timestamp, $3, $4, $5, 0) RETURNING id_post";
     constexpr auto add_comment = "add comment";

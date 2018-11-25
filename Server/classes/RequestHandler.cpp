44676b0f6ce4ac3415749cc83a939c6d20e463e0
Y2J2142 <Y2J2142@hotmail.com>
2018-11-25 14:26:03 +0100

    Qol update

--- Server/classes/RequestHandler.cpp
+++ Server/classes/RequestHandler.cpp
@@ -23,12 +23,34 @@ constexpr auto id_dumper_t = [](const json& m) {
         };
 };
 
+void handle_result(const std::optional<pqxx::result>& result, const Rest::Request& rq, Http::ResponseWriter& rw)
+{
+    if(result.has_value() && result->size() > 0)
+    {
+        rw.headers().add<Http::Header::ContentType>(MIME(Application, Json));
+        try
+        {
+            json json_array = json::parse(result->at(0).at(0).as<std::string>());
+            rw.send(Http::Code::Ok, json_array.dump().c_str());
+        } catch(json::exception& e) {
+            std::cerr << e.what() << '\n';
+            rw.send(Http::Code::Bad_Request, "Failed");        
+        }
+    }
+    else if(result.has_value())
+        rw.send(Http::Code::Bad_Request, "Failed");
+    else
+        rw.send(Http::Code::Service_Unavailable, "No Available Connectiom");
+}
+
+
+
 
 void RequestHandler::CreateUser(const Rest::Request& rq, Http::ResponseWriter rw)
 {
     json login_data = json::parse(std::move(rq.body()));
     auto dumper = string_dumper_t(login_data);
-    std::cout << "Create user request\n";
+    std::cout << QueriesConsts::find_user  <<'\n';
     auto result = pool.executeQuery(QueriesConsts::find_user, dumper("email"), dumper("username"));
     if(result.has_value() && result->size() > 0)
         rw.send(Http::Code::Bad_Request, "User Already Exists");
@@ -59,6 +81,8 @@ void RequestHandler::LoginUser(const Rest::Request& rq, Http::ResponseWriter rw)
 
     json login = json::parse(std::move(rq.body()));
     auto dumper = string_dumper_t(login);
+    std::cout << QueriesConsts::login_user  <<'\n';
+
     auto result = pool.executeQuery(QueriesConsts::login_user, dumper("login"), dumper("password"));
     if(result.has_value() && result->size() == 1)
     {
@@ -80,15 +104,12 @@ void RequestHandler::CreateMicroBlog(const Rest::Request& rq, Http::ResponseWrit
     json microblog_data = json::parse(std::move(rq.body()));
     auto dumper = string_dumper_t(microblog_data);
     auto iddumper = id_dumper_t(microblog_data);
+    std::cout << QueriesConsts::create_microblog  <<'\n';
+
     auto result = pool.executeQuery(QueriesConsts::create_microblog, dumper("title"), 
                                     iddumper("id"),dumper("tags"), dumper("private") != "private" );
 
-    if(result.has_value() && result->size() == 1)
-    {
-        rw.send(Http::Code::Ok, "MicroBlog Created");
-    }
-    else 
-        rw.send(Http::Code::Bad_Request, "Failed To Create MicroBlog");
+    handle_result(result, rq, rw);
 
 }
 
@@ -96,18 +117,9 @@ void RequestHandler::GetMyBlogs(const Rest::Request& rq, Http::ResponseWriter rw
 {
     auto id = json::parse(std::move(rq.body()));
     auto dumper = id_dumper_t(id);
-    std::cout << "Fetching microblogs\n";
+    std::cout << QueriesConsts::get_my_microblogs  <<'\n';
     auto result = pool.executeQuery(QueriesConsts::get_my_microblogs, dumper("id"));
-    if(result.has_value() && result->size() > 0)
-    {
-        rw.headers().add<Http::Header::ContentType>(MIME(Application, Json));
-
-        json json_array = json::parse(result->at(0).at(0).as<std::string>());
-        
-        rw.send(Http::Code::Ok, json_array.dump().c_str());
-    }
-    else 
-        rw.send(Http::Code::Service_Unavailable, "No Available Connection");
+    handle_result(result, rq, rw);
 
 }
 
@@ -115,20 +127,10 @@ void RequestHandler::GetPostsByBlog(const Rest::Request& rq, Http::ResponseWrite
 {
     json blog_id = json::parse(std::move(rq.body()));
     auto dumper = id_dumper_t(blog_id);
-    std::cout << "Fetching posts\n for id:" << dumper("id") << "\n";
+    std::cout << QueriesConsts::get_posts_by_id  <<'\n';
 
     auto result = pool.executeQuery(QueriesConsts::get_posts_by_id, dumper("id"));
-    if(result.has_value() && result->size() > 0)
-    {
-        rw.headers().add<Http::Header::ContentType>(MIME(Application, Json));
-
-        json json_array = json::parse(result->at(0).at(0).as<std::string>());
-
-        rw.send(Http::Code::Ok, json_array.dump().c_str());
-    }
-    else 
-        rw.send(Http::Code::Service_Unavailable, "No Available Connection");
-
+    handle_result(result, rq, rw);
 }
 
 
@@ -137,16 +139,13 @@ void RequestHandler::AddPost(const pr::Request& rq, ph::ResponseWriter rw)
     json post_data = json::parse(std::move(rq.body()));
     auto str_dumper = string_dumper_t(post_data);
     auto iddumper = id_dumper_t(post_data);
+    std::cout << QueriesConsts::add_post <<'\n';
 
-    auto result = pool.executeQuery(QueriesConsts::add_post,iddumper("author_id"), str_dumper("title"), 
+    auto result = pool.executeQuery(QueriesConsts::add_post, iddumper("author_id"), str_dumper("title"), 
                                     str_dumper("content"), iddumper("blog_id"), str_dumper("tags"));
 
-    if(result.has_value() && result->size() > 0)
-        rw.send(Http::Code::Ok, "Post Added");
-    else if(result.has_value())
-        rw.send(Http::Code::Bad_Request, "Not Able To Add Post");
-    else
-        rw.send(Http::Code::Service_Unavailable, "No Available Connection");
+    handle_result(result, rq, rw);
+    
 }
 
 void RequestHandler::AddComment(const pr::Request& rq, ph::ResponseWriter rw)
@@ -154,87 +153,55 @@ void RequestHandler::AddComment(const pr::Request& rq, ph::ResponseWriter rw)
     json comment_data = json::parse(std::move(rq.body()));
     auto str_dumper = string_dumper_t(comment_data);
     auto id_dumper = id_dumper_t(comment_data);
+    std::cout << QueriesConsts::add_comment  <<'\n';
 
     auto result = pool.executeQuery(QueriesConsts::add_comment, id_dumper("post_id"), str_dumper("content"), id_dumper("author"));
-    if(result.has_value() && result->size() > 0)
-        rw.send(Http::Code::Ok, "Comment Added");
-    else if(result.has_value())
-        rw.send(Http::Code::Bad_Request, "Not Able To Add Comment");
-    else
-        rw.send(Http::Code::Service_Unavailable, "No Available Connection");
+    handle_result(result, rq, rw);
+    
 }
 
 void RequestHandler::Upvote(const pr::Request& rq, ph::ResponseWriter rw)
 {
     json upvote = json::parse(std::move(rq.body()));
     auto dumper = id_dumper_t(upvote);
+    std::cout << QueriesConsts::upvote  <<'\n';
 
     auto result = pool.executeQuery(QueriesConsts::upvote, dumper("id_post"));
-    if(result.has_value() && result->size() > 0)
-        rw.send(Http::Code::Ok, "Upvoted");
-    else if(result.has_value())
-        rw.send(Http::Code::Bad_Request, "Could Not Upvote");
-    else
-        rw.send(Http::Code::Service_Unavailable, "No Available Connection");
+    handle_result(result, rq, rw);
+    
 }
 
 void RequestHandler::Follow(const pr::Request& rq, ph::ResponseWriter rw)
 {
     auto dumper = id_dumper_t(json::parse(std::move(rq.body())));
+    std::cout << QueriesConsts::follow  <<'\n';
 
     auto result = pool.executeQuery(QueriesConsts::follow, dumper("userid"), dumper("blogid"));
 
-    if(result.has_value() && result->size() > 0)
-        rw.send(Http::Code::Ok, "Followed");
-    else if(result.has_value())
-        rw.send(Http::Code::Bad_Request, "Could Not Follow");
-    else
-        rw.send(Http::Code::Service_Unavailable, "No Available Connection");
+    handle_result(result, rq, rw);
+
 }
 
 void RequestHandler::Followed(const pr::Request& rq, ph::ResponseWriter rw)
 {
     auto dumper = id_dumper_t(json::parse(std::move(rq.body())));
+    std::cout << QueriesConsts::followed  <<'\n';
 
     auto result = pool.executeQuery(QueriesConsts::followed, dumper("userid"));
-    if(result.has_value() && result->size() > 0)
-    {
-        rw.headers().add<Http::Header::ContentType>(MIME(Application, Json));
+    handle_result(result, rq, rw);
 
-        json json_array = json::parse(result->at(0).at(0).as<std::string>());
-
-        rw.send(Http::Code::Ok, json_array.dump().c_str());
-    }
-    else if(result.has_value())
-        rw.send(Http::Code::Bad_Request, "Could Not Find Followed Blogs");
-    else
-        rw.send(Http::Code::Service_Unavailable, "No Available Connection");
 
 }
 
 void RequestHandler::GetFollowers(const pr::Request& rq, ph::ResponseWriter rw)
 {
     auto dumper = id_dumper_t(json::parse(rq.body()));
+    std::cout << QueriesConsts::get_followers  <<'\n';
 
     auto result = pool.executeQuery(QueriesConsts::get_followers, dumper("id"));
 
-    if(result.has_value() && result->size() > 0)
-    {
-        rw.headers().add<Http::Header::ContentType>(MIME(Application, Json));
-        try
-        {
-            json json_array = json::parse(result->at(0).at(0).as<std::string>());
-            std::cout << json_array.dump() << '\n';
-            rw.send(Http::Code::Ok, json_array.dump().c_str());
-        } catch(json::exception& e) {
-            std::cerr << e.what() << '\n';
-            rw.send(Http::Code::Bad_Request, "No Followers");        
-        }
-    }
-    else if(result.has_value())
-        rw.send(Http::Code::Bad_Request, "No Followers");
-    else
-        rw.send(Http::Code::Service_Unavailable, "No Available Connectiom");
+    handle_result(result, rq, rw);
+
 }
 
 void RequestHandler::Search(const pr::Request& rq, ph::ResponseWriter rw)
@@ -251,74 +218,33 @@ void RequestHandler::Search(const pr::Request& rq, ph::ResponseWriter rw)
     else
         query = QueriesConsts::search_posts;
 
+    std::cout << query  <<'\n';
+
     auto result = pool.executeQuery(query, std::move("%"s + key + "%"s));
 
-    if(result.has_value() && result->size() > 0)
-    {
-        rw.headers().add<Http::Header::ContentType>(MIME(Application, Json));
+    handle_result(result, rq, rw);
 
-        try
-        {
-            json json_array = json::parse(result->at(0).at(0).as<std::string>());
-            rw.send(Http::Code::Ok, json_array.dump().c_str());
-        } catch(json::exception& e) {
-            std::cerr << e.what() << '\n';
-            rw.send(Http::Code::Bad_Request, "No Results");        
-        }
-    }
-    else if(result.has_value())
-        rw.send(Http::Code::Bad_Request, "No Results");
-    else
-        rw.send(Http::Code::Service_Unavailable, "No Available Connectiom");
 }
 
 void RequestHandler::GetPost(const pr::Request& rq, ph::ResponseWriter rw)
 {
     auto dumper = id_dumper_t(json::parse(std::move(rq.body())));
+    std::cout << QueriesConsts::get_post  <<'\n';
 
     auto result = pool.executeQuery(QueriesConsts::get_post, dumper("id"));
-    if(result.has_value() && result->size() > 0)
-    {
-        rw.headers().add<Http::Header::ContentType>(MIME(Application, Json));
+    handle_result(result, rq, rw);
 
-        try
-        {
-            json json_array = json::parse(result->at(0).at(0).as<std::string>());
-            rw.send(Http::Code::Ok, json_array.dump().c_str());
-        } catch(json::exception& e) {
-            std::cerr << e.what() << '\n';
-            rw.send(Http::Code::Bad_Request, "No Results");        
-        }
-    }
-    else if(result.has_value())
-        rw.send(Http::Code::Bad_Request, "No Results");
-    else
-        rw.send(Http::Code::Service_Unavailable, "No Available Connectiom");
 
 }
 
 void RequestHandler::GetComments(const pr::Request& rq, ph::ResponseWriter rw)
 {
     auto dumper = id_dumper_t(json::parse(std::move(rq.body())));
+    std::cout << QueriesConsts::get_comments  <<'\n';
 
     auto result = pool.executeQuery(QueriesConsts::get_comments, dumper("id"));
-    if(result.has_value() && result->size() > 0)
-    {
-        rw.headers().add<Http::Header::ContentType>(MIME(Application, Json));
+    handle_result(result, rq, rw);
 
-        try
-        {
-            json json_array = json::parse(result->at(0).at(0).as<std::string>());
-            rw.send(Http::Code::Ok, json_array.dump().c_str());
-        } catch(json::exception& e) {
-            std::cerr << e.what() << '\n';
-            rw.send(Http::Code::Bad_Request, "No Results");        
-        }
-    }
-    else if(result.has_value())
-        rw.send(Http::Code::Bad_Request, "No Results");
-    else
-        rw.send(Http::Code::Service_Unavailable, "No Available Connectiom");
 
 }
 

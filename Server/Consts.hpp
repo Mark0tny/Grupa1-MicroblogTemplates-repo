0b4691ed6b085c7c407d7a25677f41e1c7b2a85d
Y2J2142 <Y2J2142@hotmail.com>
2018-11-26 22:42:43 +0100

    queries update

--- Server/Consts.hpp
+++ Server/Consts.hpp
@@ -42,14 +42,14 @@ namespace QueriesConsts
     
     constexpr auto get_my_microblogs = "get users microblogs";
     constexpr auto get_my_microblogs_query = R"(SELECT COALESCE (json_agg(t), '[]'::json) FROM (
-                                                SELECT m.name, u.username, m.time_created, m.id_microblog, u.id_user, m.tags
+                                                SELECT m.name, u.username, m.time_created::timestamp(0), m.id_microblog, u.id_user, m.tags
                                                 FROM microblog m
                                                 join users u on u.id_user = m.author
                                                 WHERE u.id_user = $1) t)";
     
     constexpr auto get_posts_by_id = "get posts by id";
     constexpr auto get_posts_by_id_query = R"(SELECT COALESCE (json_agg(t), '[]'::json) FROM (
-                                                SELECT u.username, p.views, p.tags, COUNT(c.id_comment), p.time_created, p.id_post, p.id_microblog
+                                                SELECT u.username, p.views, p.tags, COUNT(c.id_comment), p.time_created::timestamp(0), p.id_post, p.id_microblog
                                                 FROM post p join users u on p.author = u.id_user
                                                 left join comments c on c.post_id = p.id_post
                                                 WHERE p.id_microblog = $1
@@ -70,28 +70,34 @@ namespace QueriesConsts
     constexpr auto get_followers_query = "SELECT COALESCE (json_agg(t), '[]'::json) FROM (SELECT username FROM users u inner join follow f on f.userid = u.id_user where f.blogid = $1) t";
     constexpr auto search_blogs = "search blogs";
     constexpr auto search_blogs_query = R"(SELECT COALESCE (json_agg(t), '[]'::json) FROM (
-            SELECT m.name, u.username, m.time_created, m.tags, m.id_microblog, m.author  
+            SELECT m.name, u.username, m.time_created::timestamp(0), m.tags, m.id_microblog, m.author  
             FROM microblog m join users u on u.id_user = m.author 
             WHERE u.username like $1 OR m.name like $1 OR m.tags like $1 
             ORDER BY m.time_created
         ) t)";
     constexpr auto search_posts = "search posts";
     constexpr auto search_posts_query = R"(SELECT COALESCE (json_agg(t), '[]'::json) FROM (
-            SELECT u.username, p.views, p.tags, COUNT(c.id_comment), p.time_created, p.id_post, p.id_microblog
+            SELECT u.username, p.views, p.tags, COUNT(c.id_comment), p.time_created::timestamp(0), p.id_post, p.id_microblog
             FROM post p join users u on p.author = u.id_user
             left join comments c on c.post_id = p.id_post
             WHERE u.username like $1 OR p.title like $1 OR p.tags like $1 or p.content like $1
-            GROUP BY u.username, p.views, p.tags, p.time_created, p.id_post, p.id_microblog
+            GROUP BY u.username, p.views, p.tags, p.time_created::timestamp(0), p.id_post, p.id_microblog
             ORDER BY p.time_created
         ) t)";
     constexpr auto get_post = "get post";
     constexpr auto get_post_query = R"(SELECT COALESCE (json_agg(t), '[]'::json) FROM (
-        SELECT * FROM post where id_post = $1
+        SELECT u.username, p.views, p.tags, p.time_created::timestamp(0), p.id_post, p.id_microblog
+        FROM post
+        JOIN users u ON u.id_user = p.author 
+        WHERE id_post = $1
         ORDER BY time_created
     ) t)";
     constexpr auto get_comments = "get comments";
     constexpr auto get_comments_query = R"(SELECT COALESCE (json_agg(t), '[]'::json) FROM (
-            SELECT * FROM comments WHERE post_id = $1
-            ORDER BY time_created
-            ) t)";
+        SELECT u.username, c.content, time_created::timestamp(0)
+	    FROM comments c 
+	    JOIN users u on u.id_user = c.author
+        WHERE c.post_id = $1
+        ORDER BY c.time_created::timestamp(0)
+        ) t)";
 }
